7efb0ab039efb9c423b3a46101ef74952bdce057
Brett Pilcher <brettpilch@yahoo.com>
2015-10-19 23:03:47 -0500

    document minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,7 +1,18 @@
+"""
+This is the main file for running the Minesweeper program.
+To play the game, from the command line type:
+
+    python minesweeper.py
+
+"""
+
 from minesweeperPG import MinesweeperPygame
 import helpers as hp
 
 def main():
+    """
+    Start a pygame version of minesweeper and enter the GUI event loop.
+    """
     game = MinesweeperPygame(hp.load_map('1'))
     game.game_loop()
 
